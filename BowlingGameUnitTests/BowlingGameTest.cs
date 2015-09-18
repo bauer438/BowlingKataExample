2b6b88adfa00264295aa911bce85ba345db74b01
Jason Sabada <jsabada@gmail.com>
2015-09-18 10:09:07 -0400

    Adding a test project and the first unit test.

---
+++ BowlingGameUnitTests/BowlingGameTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingKataExample;
+
+namespace BowlingGameUnitTests
+{
+    [TestClass]
+    public class BowlingGameTest
+    {
+        [TestMethod]
+        public void testGutterGame()
+        {
+            Game g = new Game();
+        }
+    }
+}
---
+++ BowlingKataExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKataExample
+{
+    public class Game
+    {
+        public Game()
+        {
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
